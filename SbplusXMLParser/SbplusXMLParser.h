3b23d7d4dc951baba6fcfc334fe0d284bb0b042e
Ethan Lin <ethan.lin.05@gmail.com>
2018-06-21 12:19:32 -0500

    Initial commit

---
+++ SbplusXMLParser/SbplusXMLParser.h
@@ -0,0 +1,19 @@
+//
+//  SbplusXMLParser.h
+//  SbplusXMLParser
+//
+//  Created by Ethan Lin on 6/21/18.
+//  Copyright © 2018 University of Wisconsin System Office of Academic and Student Affairs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SbplusXMLParser.
+FOUNDATION_EXPORT double SbplusXMLParserVersionNumber;
+
+//! Project version string for SbplusXMLParser.
+FOUNDATION_EXPORT const unsigned char SbplusXMLParserVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SbplusXMLParser/PublicHeader.h>
+
+
